23a4c3a613a7a34cce9caf1af406e19ccaf0511c
Sonia_Lanzarote <sonicadiz@hotmail.com>
2022-10-23 21:09:18 +0200

    En este ejercicio practicarás las estructuras de control, para ello deberás crear:
    Usando un if, crear una condición que compare si la variable numeroIf es positivo, negativo, o 0.
    Pista: Los números inferiores a 0 son negativos y los superiores, positivos.
    
    Crea un bucle While, este bucle tendrá que tener como condición que la variable numeroWhile sea inferior a 3, el bloque de código que tendrá el bucle deberá:
    
    Incrementar el valor de la variable en uno cada vez que se ejecute.
    
    Mostrarlo por pantalla cada vez que se ejecute.
    
    Para el bucle Do While, deberás crear la misma estructura que en el While, pero solo se debe ejecutar una vez.
    
    Para el bucle For, crea una variable numeroFor, esta variable tendrá como valor 0 y su condición será que la variable sea igual o menor que 3, se irá incrementando en 1 su valor cada vez que se ejecute y deberá mostrarse por pantalla.
    
    Por último, para el Switch, deberás crear la variable estacion, y distintos case para las cuatro estaciones del año. Dependiendo del valor de la variable estacion se deberá mandar un mensaje por consola informando de la estación en la que está. También habrá que poner un default para cuando el valor de la variable no sea una estación.

---
+++ src/Main.java
@@ -0,0 +1,62 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int numerolf = 3;
+
+        if (numerolf > 0) {
+            System.out.println("it's positive!");
+
+        } else if (numerolf < 0) {
+            System.out.println("it's negative!");
+
+        } else {
+            System.out.println("it's 0!");
+
+        }
+
+    }
+
+}
+
+    public static void{
+        int numberWhile = -10;
+
+        while (numberWhile < 3) {
+            System.out.println(numberWhile);
+            numberWhile = numberWhile + 1;
+        }
+    }
+}
+    public static void{
+        int numberWhile = -10;
+
+        do{
+            while (numberWhile < 3) {
+            System.out.println(numberWhile);
+            numberWhile = numberWhile + 1;
+        }
+    }
+}
+    public static void{
+        for(int numeroFor = 0; numeroFor <=3; numeroFor = numeroFor + 1){
+            system.out.println(numeroFor);
+        }
+}
+
+    public static void{
+        var estacion = "invierno";
+
+        switch (estacion){
+            case "verano":
+                system.out.println (es verano);
+
+            case "invierno":
+                system.out.println (es invierno);
+
+            case "otoño":
+                system.out.println (es otoño);
+
+            case "primavera":
+                system.out.println (es primavera);
+        }
+}
